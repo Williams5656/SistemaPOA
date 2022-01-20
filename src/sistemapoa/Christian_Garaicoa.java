5f3e4f422c9ec1f5bb7d0bf3c7e4efe3155a9d56
Christian1910n <97999837+Christian1910n@users.noreply.github.com>
2022-01-19 19:09:16 -0500

    Update Christian_Garaicoa.java

--- src/sistemapoa/Christian_Garaicoa.java
+++ src/sistemapoa/Christian_Garaicoa.java
@@ -12,8 +12,6 @@ package sistemapoa;
 public class Christian_Garaicoa {
     public static void main(String[] args) {
         System.out.println("asd");
-        System.out.println("XD vamosssssss lokoquito");
-        System.out.println("Territoria marcado por ANTHONY MOROCHO");
-        System.out.println("no creo ya XD");
+       
     }
 }

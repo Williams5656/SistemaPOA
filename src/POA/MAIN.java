17eefbc7ef531608bff7a0be9c507bf1d4c0bcf8
Usuario <Usuario@LAPTOP-P9EM33T1.mshome.net>
2022-01-24 21:33:19 -0500

    Inicio12

--- src/POA/MAIN.java
+++ src/POA/MAIN.java
@@ -4,11 +4,15 @@
  * and open the template in the editor.
  */
 package POA;
-
+import POA.Controlador.cont_inicio;
+import POA.Vista.vis_inicioSesion;
 /**
  *
  * @author WillyComputers
  */
 public class MAIN {
-    
+    public static void main(String[] args) {
+        vis_inicioSesion vista =new vis_inicioSesion();
+        cont_inicio inicio = new cont_inicio(vista);
+    }
 }

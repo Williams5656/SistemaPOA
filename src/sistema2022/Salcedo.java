cdbcc0f06c0f0413a4b1b16b9c0290ba54a16730
sebastian <sebastian@192.168.0.101>
2022-01-24 20:20:56 -0500

    Salcedo

---
+++ src/sistema2022/Salcedo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema2022;
+
+/**
+ *
+ * @author sebastian
+ */
+public class Salcedo {
+    public static void main(String[] args) {
+        System.out.println("Salcedo");
+    }
+    
+}

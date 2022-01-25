84a41d2d2ea4bc34e25c13bd405ee773c68f21bc
PC FACTORY <PC FACTORY@ALEXANDRA>
2022-01-24 20:25:33 -0500

    AlexandraVanegas

--- src/sistema2022/AlexandraV.java
+++ src/sistema2022/AlexandraV.java
@@ -8,5 +8,6 @@ package sistema2022;
 public class AlexandraV {
     public static void main(String[] args) {
         System.out.println("Alexandra Vanegas");
+        System.out.println("Hola");
     }
 }

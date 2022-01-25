bad151aaeb866c34e30c982ad18a8840ffe6c517
sebastian <sebastian@192.168.0.101>
2022-01-24 20:51:34 -0500

    Salcedo_Clase Usuario

---
+++ src/POA/Modelo/UsuarioMD.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POA.Modelo;
+
+/**
+ *
+ * @author sebastian
+ */
+public class UsuarioMD {
+    private String nombre;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    public UsuarioMD(String nombre, String nombreUsuario, String contrasenia) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+    
+}

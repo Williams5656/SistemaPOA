d8dd7301e2adbda35dd0627a006dece317340b47
Usuario <Usuario@LAPTOP-P9EM33T1.mshome.net>
2022-01-24 21:28:38 -0500

    Inicio1

--- src/POA/Controlador/cont_inicio.java
+++ src/POA/Controlador/cont_inicio.java
@@ -1,4 +1,5 @@
 package POA.Controlador;
+
 import POA.Vista.Vis_Principal;
 import POA.Vista.vis_inicioSesion;
 import POA.Vista.*;
@@ -8,25 +9,27 @@ import POA.Vista.*;
  * @author PC FACTORY
  */
 public class cont_inicio {
-     private final vis_inicioSesion vista;
+
+    private final vis_inicioSesion vista;
 
     public cont_inicio(vis_inicioSesion vista) {
         this.vista = vista;
         vista.setVisible(true);
         vista.setLocationRelativeTo(null);
-        
-        
-        //Habilitar el boton guardar
-        vista.getBtningresar().addActionListener(e->guardar());
-        
+        vista.getBtningresar().addActionListener(e -> ingresar());
+
     }
-    
-    private void guardar(){
-        vista.getTxtusuario().setText("USUARIOS");
-        Vis_Principal vistamenu = new Vis_Principal();
-        vista.setVisible(false);
+
+    private void ingresar() {
+        if (vista.getTxtusuario().getText().equals("admin") && vista.getTxtclave().getText().equals("1234")) {
+            Vis_Principal vistamenu = new Vis_Principal();
+            vista.setVisible(false);
+        }else{
+            vista.getTxtusuario().setText("");
+            vista.getTxtclave().setText("");
+        }
+        
+
     }
-     
-     
-    
+
 }

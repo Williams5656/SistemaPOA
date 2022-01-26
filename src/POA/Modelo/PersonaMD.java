0d7d9a1f040cfc9d363a44fe28866b30f8a92d69
MIGUEL <MIGUEL@192.168.1.15>
2022-01-25 21:22:30 -0500

    creacion de modelo persona

---
+++ src/POA/Modelo/PersonaMD.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POA.Modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author MIGUEL y LIZ
+ */
+public class PersonaMD {
+    
+    private String cedula;
+    private String nombres;
+    private String apellidos;
+    private LocalDate fecha_nacimiento;
+    private String direccion;
+    private String correo;
+    private String telefono;
+    private byte[] foto;
+    
+
+    public PersonaMD() {
+    }
+
+    public PersonaMD(String cedula, String nombres, String apellidos, LocalDate fecha_nacimiento, String direccion, String correo, String telefono, byte[] foto) {
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.foto = foto;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public LocalDate getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+    
+
+    
+}

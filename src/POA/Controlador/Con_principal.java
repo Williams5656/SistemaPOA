c6995874864cafed3a607a3acc479328a14788a8
USUARIO <USUARIO@DESKTOP-M87A66D>
2022-01-27 21:05:29 -0500

    activar ventanas

--- src/POA/Controlador/Con_principal.java
+++ src/POA/Controlador/Con_principal.java
@@ -5,13 +5,9 @@
  */
 package POA.Controlador;
 
-import POA.Vista.Vis_Asignacion;
-import POA.Vista.Vis_Perfil;
-import POA.Vista.Vis_Principal;
-import POA.Vista.Vis_Roles;
-import POA.Vista.Vis_Usuario;
-import POA.Vista.vis_inicioSesion;
+import POA.Vista.*;
 import java.awt.Dimension;
+import static java.awt.Frame.MAXIMIZED_BOTH;
 
 /**
  *
@@ -25,54 +21,56 @@ public class Con_principal {
         this.vista = vista;
         vista.setVisible(true);
         vista.setLocationRelativeTo(null);
+        vista.setExtendedState(MAXIMIZED_BOTH);
         vista.getBtn_perfil().addActionListener(e -> perfil());
-        vista.getBtn_asignacion().addActionListener(e -> asignacion());
-        vista.getBtn_roles().addActionListener(e -> roles());
-        vista.getBtn_salir().addActionListener(e -> salir());
-        vista.getBtn_nuevo().addActionListener(e -> nuevo());
+//        vista.getBtn_asignacion().addActionListener(e -> asignacion());
+//        vista.getBtn_roles().addActionListener(e -> roles());
+//        vista.getBtn_salir().addActionListener(e -> salir());
+//        vista.getBtn_nuevo().addActionListener(e -> nuevo());
         
 
     }
 
     private void perfil() {
-        Vis_Perfil user = new Vis_Perfil();
-        vista.getESCRITORIO().add(user);
+        Vis_Perfil perfil = new Vis_Perfil();
+        vista.getESCRITORIO().add(perfil);
         Dimension desktopSize = vista.getESCRITORIO().getSize();
-        Dimension FrameSize = user.getSize();
-        user.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
-        user.show();
+        Dimension FrameSize = perfil.getSize();
+        perfil.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
+        Con_perfil per = new Con_perfil(perfil);
+        
     }
 
-    private void asignacion() {
-        Vis_Asignacion user = new Vis_Asignacion();
-        vista.getESCRITORIO().add(user);
-        Dimension desktopSize = vista.getESCRITORIO().getSize();
-        Dimension FrameSize = user.getSize();
-        user.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
-        user.show();
-    }
-
-    private void roles() {
-        Vis_Roles user = new Vis_Roles();
-        vista.getESCRITORIO().add(user);
-        Dimension desktopSize = vista.getESCRITORIO().getSize();
-        Dimension FrameSize = user.getSize();
-        user.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
-        user.show();
-    }
-
-    private void salir() {
-        this.vista.getESCRITORIO().setVisible(false);
-        vis_inicioSesion user = new vis_inicioSesion();
-        user.setVisible(true);
-    }
-
-    private void nuevo() {
-        Vis_Usuario user = new Vis_Usuario();
-        vista.getESCRITORIO().add(user);
-        Dimension desktopSize = vista.getESCRITORIO().getSize();
-        Dimension FrameSize = user.getSize();
-        user.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
-        user.show();
-    }
+//    private void asignacion() {
+//        Vis_Asignacion user = new Vis_Asignacion();
+//        vista.getESCRITORIO().add(user);
+//        Dimension desktopSize = vista.getESCRITORIO().getSize();
+//        Dimension FrameSize = user.getSize();
+//        user.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
+//        user.show();
+//    }
+//
+//    private void roles() {
+//        Vis_Roles user = new Vis_Roles();
+//        vista.getESCRITORIO().add(user);
+//        Dimension desktopSize = vista.getESCRITORIO().getSize();
+//        Dimension FrameSize = user.getSize();
+//        user.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
+//        user.show();
+//    }
+//
+//    private void salir() {
+//        this.vista.getESCRITORIO().setVisible(false);
+//        vis_inicioSesion user = new vis_inicioSesion();
+//        user.setVisible(true);
+//    }
+//
+//    private void nuevo() {
+//        Vis_Usuario user = new Vis_Usuario();
+//        vista.getESCRITORIO().add(user);
+//        Dimension desktopSize = vista.getESCRITORIO().getSize();
+//        Dimension FrameSize = user.getSize();
+//        user.setLocation((desktopSize.width - FrameSize.width) / 2, (desktopSize.height - FrameSize.height) / 2);
+//        user.show();
+//    }
 }

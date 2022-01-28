bfe8e1bcce6cb871318c82365b8953d6dd7b45e9
USUARIO <USUARIO@DESKTOP-M87A66D>
2022-01-27 21:07:03 -0500

    activar ventanas

--- src/POA/Vista/Vis_Principal.java
+++ src/POA/Vista/Vis_Principal.java
@@ -5,7 +5,7 @@
  */
 package POA.Vista;
 
-import java.awt.Dimension;
+
 import javax.swing.JDesktopPane;
 import javax.swing.JMenuItem;
 
@@ -20,7 +20,7 @@ public class Vis_Principal extends javax.swing.JFrame {
      */
     public Vis_Principal() {
         initComponents();
-        this.setExtendedState(Vis_Principal.MAXIMIZED_BOTH);
+        
     }
 
     public JMenuItem getBtn_nuevo() {
@@ -195,38 +195,7 @@ public class Vis_Principal extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Vis_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Vis_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Vis_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Vis_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Vis_Principal().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane ESCRITORIO;

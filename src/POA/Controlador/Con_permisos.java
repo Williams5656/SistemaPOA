871d2c0d389e35e7a439cb2096be27f1cfdcbb4b
IsaacMejia <IsaacMejia@192.168.1.118>
2022-02-20 17:43:54 -0500

    botn permisos

---
+++ src/POA/Controlador/Con_permisos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POA.Controlador;
+
+import POA.Vista.Vis_Permisos;
+
+/**
+ *
+ * @author IsaacMejia
+ */
+public class Con_permisos {
+    private final Vis_Permisos vista;
+
+    public Con_permisos(Vis_Permisos vista) {
+        this.vista = vista;
+        vista.setVisible(true);
+    }
+    
+    
+}
